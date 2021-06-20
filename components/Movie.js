42c6d2cb9cf1c18640497853ca007b025f4197a4
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-20 13:10:41 +0530

    movie delete functionality added

--- components/Movie.js
+++ components/Movie.js
@@ -1,6 +1,24 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function ({ movie }) {
+  const router = useRouter();
+  const btnStyles = {
+    margin: "0.6rem",
+    backgroundColor: "#f00",
+    color: "#fff",
+    padding: "0.5rem",
+    borderRadius: "0.7rem",
+  };
+
+  const deleteMovie = async () => {
+    await fetch(`/api/movies/?id=${movie.id}`, {
+      method: "DELETE",
+    });
+
+    router.push("/");
+  };
+
   return (
     <li key={movie.id}>
       <span>
@@ -9,6 +27,9 @@ export default function ({ movie }) {
       <span>{movie.year}</span>
       <span>{movie.description}</span>
       <Link href={`/movies/${movie.slug}`}>More about this movie</Link>
+      <button style={btnStyles} type="button" onClick={deleteMovie}>
+        Delete movie
+      </button>
     </li>
   );
 }
