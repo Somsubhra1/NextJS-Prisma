265d864c57f5518d362f64c1779bcbb878046452
Somsubhra Das <das.somsubhra1@gmail.com>
2021-06-20 12:09:17 +0530

    form layout created

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
 const { movie: Movie } = new PrismaClient();
 
 export default function Home({ data }) {
+  const [formData, setFormData] = useState({});
+
+  const saveMovie = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -25,6 +32,42 @@ export default function Home({ data }) {
             </li>
           ))}
         </ul>
+
+        <form className={styles.movieform} onSubmit={saveMovie}>
+          <input
+            type="text"
+            placeholder="Title"
+            name="title"
+            onChange={(e) =>
+              setFormData({ ...formData, title: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Year"
+            name="year"
+            onChange={(e) =>
+              setFormData({ ...formData, year: +e.target.value })
+            }
+          />
+          <textarea
+            name="description"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Description"
+            onChange={(e) =>
+              setFormData({ ...formData, description: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Slug"
+            name="slug"
+            onChange={(e) => setFormData({ ...formData, slug: e.target.value })}
+          />
+          <button type="submit">Add movie</button>
+        </form>
       </main>
     </div>
   );
